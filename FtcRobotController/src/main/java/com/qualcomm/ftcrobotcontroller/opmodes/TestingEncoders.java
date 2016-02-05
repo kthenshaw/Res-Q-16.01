2ed90e73981577c6d31b9fa7d467123621fae241
kthenshaw <kthenshaw@gmail.com>
2016-02-05 10:23:38 -0500

    Working on Gyro

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestingEncoders.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestingEncoders.java
@@ -17,7 +17,7 @@ public class TestingEncoders  extends OpMode {
     //Calls the two subclasses containing the separate functions used to control all the functions of the robot
     TeleopTank TeleopTank;
     Controller2 Controller2;
-    AdafruitIMU boschBNO055;
+    Bno055 Bno055;
 
 
     //leftDrive, rightDrive, and balance are all actually two DC Motors
@@ -46,14 +46,6 @@ public class TestingEncoders  extends OpMode {
     double allClearHit = .05;
     double allClearOpen = .95;
 
-    //This creates the array which the gyro sensor will write into
-    //thunderA B and C will then be used to output the roll, pitch, and yaw using telemetry
-    volatile double[] rollAngle = new double[2], pitchAngle = new double[2], yawAngle = new double[2];
-    double thunderA;
-    double thunderB;
-    double thunderC;
-
-    long systemTime;//Relevant values of System.nanoTime
 
 
     @Override
@@ -70,55 +62,37 @@ public class TestingEncoders  extends OpMode {
         //leftDrive.setDirection(DcMotor.Direction.REVERSE);
         TeleopTank = new TeleopTank(hardwareMap.dcMotor.get("left_drive"),hardwareMap.dcMotor.get("right_drive"));
         Controller2 = new Controller2();
+        Bno055 = new Bno055(hardwareMap,("bno055"));
 
         //sets initial servo positions
         blueClimb.setPosition(blueClosed);
         redClimb.setPosition(redClosed);
-
-        systemTime = System.nanoTime();
-        try
-        {
-            boschBNO055 = new AdafruitIMU(hardwareMap, "bno055", (byte)(AdafruitIMU.BNO055_ADDRESS_A * 2), (byte)AdafruitIMU.OPERATION_MODE_IMU);
-        }
-        catch (RobotCoreException e)
-        {
-            Log.i("FtcRobotController", "Exception: " + e.getMessage());
-        }
-        Log.i("FtcRobotController", "IMU Init method finished in: " + (-(systemTime - (systemTime = System.nanoTime()))) + " ns.");
-
+        Bno055.init();
     }
 
     @Override
-    public void start()
+    public void init_loop()
     {
-        systemTime = System.nanoTime();
-        boschBNO055.startIMU();//Set up the IMU as needed for a continual stream of I2C reads.
-        Log.i("FtcRobotController", "IMU Start method finished in: "
-                + (-(systemTime - (systemTime = System.nanoTime()))) + " ns.");
+        Bno055.init_loop();
+        if (Bno055.isInitDone())
+        {
+            telemetry.addData("","Ready to start");
+        }
     }
+
 
 
     @Override
     public void loop()
     {
-        if (gamepad1.a)
-        {
-            leftDrive.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-            rightDrive.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-        }
-        else
-        {
-            TeleopTank.Tank(gamepad1.left_stick_y, gamepad1.right_stick_y);
-            boschBNO055.getIMUGyroAngles(rollAngle,pitchAngle,yawAngle);
-            telemetry.addData("Right Wheel: ", gamepad1.right_stick_y);
-            telemetry.addData("Left Wheel: ", gamepad1.left_stick_y);
-            telemetry.addData("Right Encoder: ", rightDrive.getCurrentPosition());
-            telemetry.addData("Left Encoder: ", leftDrive.getCurrentPosition());
-            telemetry.addData("Headings(yaw): ", String.format("Euler= %4.5f, Quaternion calculated= %4.5f", yawAngle[0], yawAngle[1]));
-            telemetry.addData("Pitches: ", String.format("Euler= %4.5f, Quaternion calculated= %4.5f", pitchAngle[0], pitchAngle[1]));
-            telemetry.addData("Max I2C read interval: ", String.format("%4.4f ms. Average interval: %4.4f ms.", boschBNO055.maxReadInterval, boschBNO055.avgReadInterval));
-
-        }
-
+        Bno055.loop();
+        TeleopTank.Tank(gamepad1.left_stick_y, gamepad1.right_stick_y);
+        telemetry.addData("Right Wheel: ", gamepad1.right_stick_y);
+        telemetry.addData("Left Wheel: ", gamepad1.left_stick_y);
+        telemetry.addData("Right Encoder: ", rightDrive.getCurrentPosition());
+        telemetry.addData("Left Encoder: ", leftDrive.getCurrentPosition());
+        telemetry.addData("Euler X: ", Bno055.eulerX());
+        telemetry.addData("Euler Y: ", Bno055.eulerY());
+        telemetry.addData("Euler Z: ", Bno055.eulerZ());
     }
 }
